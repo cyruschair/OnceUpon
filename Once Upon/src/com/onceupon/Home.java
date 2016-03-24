58fb0f98fa9dc2f2a9e65aa8f2dbc96ae9c71fdf
brivera <brivera@imoderate.com>
2016-03-24 09:04:09 -0600

    start commit

---
+++ Once Upon/src/com/onceupon/Home.java
@@ -0,0 +1,12 @@
+package com.onceupon;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Home extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
